85ac3e50172f18be97747dc5631d76496d5a2f6c
pasinduweb <algopasindu@gmail.com>
2025-06-18 21:34:19 +0530

    Implement ErrorBoundary for PastOrdersRoute

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,35 @@
+import { Component } from "react";
+import { Link } from "@tanstack/react-router";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error-boundary">
+          <h2>oh!</h2>
+          <p>
+            There was an error with this listing. <Link to="/">Click here</Link> to back to the home page.
+          </p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
+
+/*
+ * Class component for error boundaries.
+ * https://legacy.reactjs.org/docs/error-boundaries.html
+ */
